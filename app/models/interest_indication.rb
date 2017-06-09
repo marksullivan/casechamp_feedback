87a1d8f07098d525185d792a940e80a5086a3d1e
Mark Sullivan <sullivan.mark.r@gmail.com>
2017-06-08 23:31:10 -0700

    most recent updates

--- app/models/interest_indication.rb
+++ app/models/interest_indication.rb
@@ -2,4 +2,8 @@ class InterestIndication < ApplicationRecord
 
   has_one :interest_outcome, :class_name => "Response", :dependent => :nullify
 
+
+  validates :competition_name, :presence => true
+  validates :email_address, :presence => true, :uniqueness => { :scope => :competition_name }
+
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -2,4 +2,8 @@ class Response < ApplicationRecord
 
 belongs_to :interest_indication
 
+validates :interest_indication_id, :presence => true, :uniqueness => true
+validates :outcome, :presence => true
+validates :likely_to_recommend, :presence => true
+
 end
